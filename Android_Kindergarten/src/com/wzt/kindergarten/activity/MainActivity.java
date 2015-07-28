0aacdea6f2e9b116c6e658ac94fdcb8855a451f6
sunmengliang <liangxxmeng@163.com>
2015-07-28 18:27:59 +0800

    07.28 office 18.27

--- Android_Kindergarten/src/com/wzt/kindergarten/activity/MainActivity.java
+++ Android_Kindergarten/src/com/wzt/kindergarten/activity/MainActivity.java
@@ -1,36 +1,176 @@
 package com.wzt.kindergarten.activity;
 
-import com.example.android_kindergarten.R;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import com.example.android_kindergarten.R;
+import com.wzt.kindergarten.BaseApp;
+import com.wzt.kindergarten.fragment.InformFragment;
+import com.wzt.kindergarten.fragment.InteractionFragment;
+import com.wzt.kindergarten.fragment.KindergartenFragment;
+import com.wzt.kindergarten.fragment.MeFragment;
+import com.wzt.kindergarten.fragment.ParadiseFragment;
+import com.wzt.kindergarten.netstate.NetworkStateReceiver;
+
+public class MainActivity extends FragmentActivity {
+	// 第一次点击返回键的时间
+	private long firstTime;
+	// fragment管理类
+	private FragmentManager fm;
+	// 五个Fragment界面
+	private Fragment[] fragments = new Fragment[5];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// 动画效果淡入淡出
+		overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
+		initView();
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	/**
+	 * 初始化视图
+	 */
+	private void initView() {
+		fm = getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		KindergartenFragment kindergartendFragment = new KindergartenFragment();
+		fragments[0] = kindergartendFragment;
+		ft.add(R.id.linear_container, kindergartendFragment);
+		ft.commit();
+
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	// radiogroup点击事件
+	public void rbClick(View view) {
+		switch (view.getId()) {
+		case R.id.bottom_rb_kindergarten:
+			// 幼儿园
+			showFragment(0);
+			hideOtherFragment(0);
+			break;
+		case R.id.bottom_rb_inform:
+			// 通知
+			showFragment(1);
+			hideOtherFragment(1);
+			break;
+		case R.id.bottom_rb_interaction:
+			// 互动
+			showFragment(2);
+			hideOtherFragment(2);
+			break;
+		case R.id.bottom_rb_paradise:
+			// 乐园
+			showFragment(3);
+			hideOtherFragment(3);
+			break;
+		// 我
+		case R.id.bottom_rb_me:
+			showFragment(4);
+			hideOtherFragment(4);
+			break;
+
+		default:
+			break;
 		}
-		return super.onOptionsItemSelected(item);
 	}
+
+	/**
+	 * Fragment的显示
+	 * 
+	 * @param index
+	 */
+	private void showFragment(int index) {
+		FragmentTransaction ft = fm.beginTransaction();
+		switch (index) {
+		case 0:
+			// 幼儿园
+			if (fragments[0] == null) {
+				fragments[0] = new KindergartenFragment();
+				ft.add(R.id.linear_container, fragments[0]);
+			} else {
+				ft.show(fragments[0]);
+			}
+			ft.commit();
+			break;
+		case 1:
+			// 通知
+			if (fragments[1] == null) {
+				fragments[1] = new InformFragment();
+				ft.add(R.id.linear_container, fragments[1]);
+			} else {
+				ft.show(fragments[1]);
+			}
+			ft.commit();
+			break;
+		case 2:
+			// 互动
+			if (fragments[2] == null) {
+				fragments[2] = new InteractionFragment();
+				ft.add(R.id.linear_container, fragments[2]);
+			} else {
+				ft.show(fragments[2]);
+			}
+			ft.commit();
+
+			break;
+		case 3:
+			// 乐园
+			if (fragments[3] == null) {
+				fragments[3] = new ParadiseFragment();
+				ft.add(R.id.linear_container, fragments[3]);
+			} else {
+				ft.show(fragments[3]);
+			}
+			ft.commit();
+			break;
+		case 4:
+			// 我
+			if (fragments[4] == null) {
+				fragments[4] = new MeFragment();
+				ft.add(R.id.linear_container, fragments[4]);
+			} else {
+				ft.show(fragments[4]);
+			}
+			ft.commit();
+			break;
+		}
+	}
+
+	/**
+	 * Fragment的隐藏
+	 * 
+	 * @param index
+	 */
+	private void hideOtherFragment(int index) {
+		for (int i = 0; i < fragments.length; i++) {
+			if (i != index && fragments[i] != null) {
+				FragmentTransaction ft = fm.beginTransaction();
+				ft.hide(fragments[i]);
+				ft.commit();
+			}
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		long secondTime = System.currentTimeMillis();
+		if (secondTime - firstTime > 2000) { // 如果两次按键时间间隔大于2秒，则不退出
+			BaseApp.getInstance().showToast("再按一次退出程序");
+			firstTime = secondTime;// 更新firstTime
+			return;
+		} else {
+			// 两次按键小于2秒时，退出应用
+			finish();
+			NetworkStateReceiver.unRegisterNetworkStateReceiver(BaseApp
+					.getInstance());
+		}
+	}
+
 }
---
+++ Android_Kindergarten/src/com/wzt/kindergarten/fragment/InformFragment.java
@@ -0,0 +1,23 @@
+package com.wzt.kindergarten.fragment;
+
+import com.example.android_kindergarten.R;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InformFragment extends Fragment {
+	
+	@Override
+	@Nullable
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.fragment_first_inform, null);
+		return view;
+	}
+
+}
